6cfacb21704a75df229f8182185a88724691aece
ferwirrow <ferkedezma95@gmail.com>
2025-02-06 19:33:15 -0600

    [any message]

--- src/main.cs
+++ src/main.cs
@@ -1,15 +1,15 @@
 using System.Net;
 using System.Net.Sockets;
 
-string command;
+string command = "";
 bool invalid = false;
 
-//Uncomment this line to pass the first stage
-Console.Write("$ ");
-
-// Wait for user input
-command = Console.ReadLine();
 
 
 
-if(invalid== false) Console.WriteLine($"{command}: command not found");
+while (command != "quit()")
+{
+    Console.Write("$ ");
+    command = Console.ReadLine();
+    if(invalid== false) Console.WriteLine($"{command}: command not found");
+}
