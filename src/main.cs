3afafb098e6990f5732f9615d8b5ad3e70005265
ferwirrow <ferkedezma95@gmail.com>
2025-02-10 23:30:10 -0600

    [any message]

--- src/main.cs
+++ src/main.cs
@@ -89,9 +89,9 @@ namespace HolaMundo
     static void wordToList( string input ){  // convierte el texto a lista de words
         words_command.Clear();
          
-         bool singleQuoting = false;
+        bool singleQuoting = false;
         bool doubleQuoting = false;
-        bool escapeNext = false;
+        bool escapeNext = false; // Controla si el siguiente carácter debe ser un carácter literal (como una barra invertida)
 
         string wordfinal = "";
 
